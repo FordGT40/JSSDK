52de180339374b976e8131e3bba3f7f361bd9165
hanxuefeng <1007214131@qq.com>
2022-05-09 17:26:22 +0800

    屏蔽返回键逻辑18

--- jsinterfacelib/src/main/java/com/wisdom/jsinterfacelib/handler/HookNavigationBackActionHandler.java
+++ jsinterfacelib/src/main/java/com/wisdom/jsinterfacelib/handler/HookNavigationBackActionHandler.java
@@ -36,11 +36,13 @@ public class HookNavigationBackActionHandler extends BridgeHandler {
                 } else {
                     String newData = data.substring(1, data.length() - 1);
                     String jsFunction = "javascript:" + newData + "()";
+                    LogUtils.i("接到的js方法："+jsFunction);
                     ConstantString.JS_FUN_NAME = jsFunction;
                     //获取页面的webView
                     BridgeWebView webView = ((AppCompatActivity) context).findViewById(R.id.wv_webview);
                     //屏蔽上导航的返回按钮
                     ImageView back = ((AppCompatActivity) context).findViewById(R.id.back);
+                    /**
                     if (back != null) {
                         back.setOnClickListener(view -> {
                             //什么都不做
@@ -48,6 +50,7 @@ public class HookNavigationBackActionHandler extends BridgeHandler {
                             webView.loadUrl(jsFunction);
                         });
                     }
+                     **/
                     //屏蔽物理返回键
                     CAN_BACK_KEY_USEFUL = true;
                     BaseModel baseModel = new BaseModel("操作成功", 0, "操作成功");
